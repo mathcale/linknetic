cd0f37153daa45a9aa57869fc32b42242faa4261
Matheus Calegaro <hello@matheus.me>
2022-04-29 22:49:20 -0300

    feat(home): add signin button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
+import Link from 'next/link';
+
 export default function IndexPage() {
   return (
     <div>
       <h1 className="text-6xl">Home</h1>
+
+      <Link href="/auth">
+        <a className="btn">Sign in</a>
+      </Link>
     </div>
   );
 }
