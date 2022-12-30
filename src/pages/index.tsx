27cc6733ad16f8c41d4afdb97cbeea216d47757f
Matheus Calegaro <hello@matheus.me>
2022-12-30 17:40:46 -0300

    chore(index): WIP landing page design

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,40 @@
 import Link from 'next/link';
 
-export default function IndexPage() {
-  return (
-    <div>
-      <h1 className="text-6xl">Home</h1>
+import Navbar from '../components/Navbar/navbar.component';
+import AuthUser from '../hooks/auth-user.hook';
 
-      <Link href="/auth">
-        <a className="btn">Sign in</a>
-      </Link>
+import { supabase } from '../utils/supabase.util';
+
+export default function IndexPage() {
+  const user = AuthUser();
+
+  return (
+    <div className="container mx-auto px-4 py-4">
+      <Navbar title="Linknetic" user={user} />
+
+      <div className="prose mt-10">
+        <h1 className="text-5xl mb-5">Cool title here</h1>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum magni fuga amet ex quod
+          error ullam nostrum blanditiis nemo aut sed recusandae, tenetur dignissimos repellat
+          necessitatibus fugit beatae molestias? Animi.
+        </p>
+
+        <Link href={user ? '/dashboard' : '/auth'} passHref>
+          <a className="btn btn-md">Start now</a>
+        </Link>
+      </div>
     </div>
   );
+}
+
+export async function getServerSideProps({ req }) {
+  const { user } = await supabase.auth.api.getUserByCookie(req);
+
+  return {
+    props: {
+      user,
+    },
+  };
 }
