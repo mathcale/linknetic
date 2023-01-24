973abb792fe0e77d66c496dbfd85f8b3d16159b1
Matheus Calegaro <hello@matheus.me>
2023-01-24 19:10:01 -0300

    feat(components): add share page button

---
+++ src/components/ShareButton/share-button.component.tsx
@@ -0,0 +1,35 @@
+import toast from 'react-hot-toast';
+
+interface ShareButtonProps {
+  url: string;
+}
+
+export default function ShareButton({ url }: ShareButtonProps) {
+  const onShareButtonPress = (): void => {
+    navigator.clipboard.writeText(url);
+    toast.success('Page link copied to clipboard!');
+  };
+
+  return (
+    <button
+      className="btn gap-2 fixed z-90 bottom-5 right-5 sm:bottom-10 sm:right-10"
+      onClick={onShareButtonPress}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-6 h-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M7.217 10.907a2.25 2.25 0 100 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186l9.566-5.314m-9.566 7.5l9.566 5.314m0 0a2.25 2.25 0 103.935 2.186 2.25 2.25 0 00-3.935-2.186zm0-12.814a2.25 2.25 0 103.933-2.185 2.25 2.25 0 00-3.933 2.185z"
+        />
+      </svg>
+      Share
+    </button>
+  );
+}
