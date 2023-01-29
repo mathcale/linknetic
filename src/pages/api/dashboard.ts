6e5fdeff3ab861a9625fa29df6872714bbb3ce12
Matheus Calegaro <hello@matheus.me>
2023-01-29 00:48:36 -0300

    fix(api/dashboard): use empty array for links when page doesn't exists

--- src/pages/api/dashboard.ts
+++ src/pages/api/dashboard.ts
@@ -29,7 +29,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
   if (page.length === 0) {
     return res.status(200).json({
       total: 0,
-      links: null,
+      links: [],
     });
   }
 
