31334e0fe520362d9b9f2dd2522fdd63384fbfd7
Matheus Calegaro <hello@matheus.me>
2023-01-29 00:51:18 -0300

    chore(auth): remove twitter auth method

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -3,7 +3,7 @@ import Navbar from '../components/Navbar/navbar.component';
 import { AuthRedirect } from '../hooks/auth-user.hook';
 import { supabase } from '../utils/supabase.util';
 
-type SignInProvider = 'facebook' | 'google' | 'twitter';
+type SignInProvider = 'facebook' | 'google';
 
 export default function IndexPage() {
   AuthRedirect();
@@ -27,10 +27,6 @@ export default function IndexPage() {
 
         <button type="button" className="btn mb-5" onClick={e => onSignInButtonClick('google', e)}>
           Sign in with Google
-        </button>
-
-        <button type="button" className="btn mb-5" onClick={e => onSignInButtonClick('twitter', e)}>
-          Sign in with Twitter
         </button>
       </div>
     </div>
